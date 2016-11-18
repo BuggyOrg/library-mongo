32ee36c3d69895cecbbec453bed7e310e5d87f0c
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-11-18 14:31:24 +0100

    (feat) Log start errors.

--- src/server.js
+++ src/server.js
@@ -4,3 +4,5 @@ import {serve} from './api'
 const port = process.env.BUGGY_LIBRARY_PORT || 8818
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost'
 serve(port, mongoUrl)
+.then(() => console.log(`Listening on port ${port}`))
+.catch((err) => console.error('Could not start the server', err))
